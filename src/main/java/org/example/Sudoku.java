b1be42f353e7c20fc57f0c591d355a37be8f9116
Bartlomiej <szybiakbartlomiej@gmail.com>
2023-03-07 14:35:26 +0100

    change of printing for better readability

--- src/main/java/org/example/Sudoku.java
+++ src/main/java/org/example/Sudoku.java
@@ -27,10 +27,17 @@ public class Sudoku {
 
     private static void printBoard(int[][] board) {
         for (int row=0;row<GRID_SIZE;row++){
+            if(row %3==0 && row !=0 ){
+                System.out.println("__________");
+            }
             for (int column=0;column<GRID_SIZE;column++){
+                if (column%3==0 && column!=0){
+                    System.out.print("|");
+                }
                 System.out.print(board[row][column]);
             }
             System.out.println();
+
         }
     }
 
